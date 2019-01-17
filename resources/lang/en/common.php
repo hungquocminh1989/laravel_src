f56694be11befd4a27d4914016219a6820f87582
Minh <hungquocminh1989@gmail.com>
2019-01-17 15:33:59 +0700

    Add lang en

---
+++ resources/lang/en/common.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'popup_button_ok' => 'OK',
+    'popup_button_close' => 'Close',
+    'popup_title_success' => 'Notify',
+    'popup_title_confirm' => 'Confirm Message',
+    'popup_title_error' => 'Warning',
+    
+];
